330ffb97b391726e93c796d6ec9b91a1e286ac07
Tharindu Dasun <tharindudasun0@gmail.com>
2021-08-14 01:19:17 +0530

    Created the pharmacy store activity

--- app/src/main/java/com/example/aspirushealthcareandroidapp/PharmcyStore.java
+++ app/src/main/java/com/example/aspirushealthcareandroidapp/PharmcyStore.java
@@ -10,6 +10,5 @@ public class PharmcyStore extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pharmcy_store);
-
     }
 }
