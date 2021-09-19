234010bb1bd440953f3460f1b70f1ff20cbbe51a
Minindu Senadheera <69930656+MininduSenadheera@users.noreply.github.com>
2021-09-19 22:56:47 +0530

    linked the views in patient sign up

--- app/src/main/java/com/example/aspirushealthcareandroidapp/PatientSignUp.java
+++ app/src/main/java/com/example/aspirushealthcareandroidapp/PatientSignUp.java
@@ -17,34 +17,34 @@ import java.util.Calendar;
 public class PatientSignUp extends AppCompatActivity {
 
     DatePickerDialog calendar;
-    TextInputLayout username;
-    TextInputLayout email;
-    TextInputLayout phone;
-    TextInputLayout dob;
-    RadioButton male;
-    RadioButton female;
-    TextInputLayout password;
-    TextInputLayout confirmPassword;
-    Button signup;
+    TextInputLayout et_username;
+    TextInputLayout et_email;
+    TextInputLayout et_phone;
+    TextInputLayout et_dob;
+    RadioButton rbtn_male;
+    RadioButton rbtn_female;
+    TextInputLayout et_password;
+    TextInputLayout et_confirmPassword;
+    Button btn_signup;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_patient_sign_up);
 
-        username = findViewById(R.id.signup_username);
-        email = findViewById(R.id.signup_email);
-        phone = findViewById(R.id.signup_phone);
-        dob = findViewById(R.id.signup_dob);
-        male = findViewById(R.id.rbtn_male);
-        female = findViewById(R.id.rbtn_female);
-        password = findViewById(R.id.signup_password);
-        confirmPassword = findViewById(R.id.signup_confirm_pw);
-        signup = findViewById(R.id.btn_signup);
+        et_username = findViewById(R.id.signup_username);
+        et_email = findViewById(R.id.signup_email);
+        et_phone = findViewById(R.id.signup_phone);
+        et_dob = findViewById(R.id.signup_dob);
+        rbtn_male = findViewById(R.id.rbtn_male);
+        rbtn_female = findViewById(R.id.rbtn_female);
+        et_password = findViewById(R.id.signup_password);
+        et_confirmPassword = findViewById(R.id.signup_confirm_pw);
+        btn_signup = findViewById(R.id.btn_signup);
 
         //date picker popup
-        dob.getEditText().setInputType(InputType.TYPE_NULL);
-        dob.getEditText().setOnClickListener(new View.OnClickListener() {
+        et_dob.getEditText().setInputType(InputType.TYPE_NULL);
+        et_dob.getEditText().setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 final Calendar cldr = Calendar.getInstance();
@@ -55,7 +55,7 @@ public class PatientSignUp extends AppCompatActivity {
                 calendar = new DatePickerDialog(PatientSignUp.this, new DatePickerDialog.OnDateSetListener() {
                     @Override
                     public void onDateSet(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
-                        dob.getEditText().setText(dayOfMonth + "/" + (monthOfYear + 1) + "/" + year);
+                        et_dob.getEditText().setText(dayOfMonth + "/" + (monthOfYear + 1) + "/" + year);
                     }
                 }, year, month, day);
                 calendar.show();
