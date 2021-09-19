898ccecf0fe41fd90aaccbb5af7f7846fd1e4844
Minindu Senadheera <69930656+MininduSenadheera@users.noreply.github.com>
2021-09-19 20:11:04 +0530

    linked the views

--- app/src/main/java/com/example/aspirushealthcareandroidapp/PatientSignUp.java
+++ app/src/main/java/com/example/aspirushealthcareandroidapp/PatientSignUp.java
@@ -7,6 +7,8 @@ import android.os.Bundle;
 import android.text.InputType;
 import android.view.View;
 import android.widget.DatePicker;
+import android.widget.RadioButton;
+
 import com.google.android.material.textfield.TextInputLayout;
 
 import java.util.Calendar;
@@ -14,15 +16,30 @@ import java.util.Calendar;
 public class PatientSignUp extends AppCompatActivity {
 
     DatePickerDialog calendar;
+    TextInputLayout username;
+    TextInputLayout email;
+    TextInputLayout phone;
     TextInputLayout dob;
+    RadioButton male;
+    RadioButton female;
+    TextInputLayout password;
+    TextInputLayout confirmPassword;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_patient_sign_up);
 
-        //date picker popup
+        username = findViewById(R.id.signup_username);
+        email = findViewById(R.id.signup_email);
+        phone = findViewById(R.id.signup_phone);
         dob = findViewById(R.id.signup_dob);
+        male = findViewById(R.id.rbtn_male);
+        female = findViewById(R.id.rbtn_female);
+        password = findViewById(R.id.signup_password);
+        confirmPassword = findViewById(R.id.signup_confirm_pw);
+
+        //date picker popup
         dob.getEditText().setInputType(InputType.TYPE_NULL);
         dob.getEditText().setOnClickListener(new View.OnClickListener() {
             @Override
