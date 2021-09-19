d2579da0148af6899b0bd1f2fd0afdc8a926ba65
Minindu Senadheera <69930656+MininduSenadheera@users.noreply.github.com>
2021-09-19 19:44:23 +0530

    created the calendar popup

---
+++ app/src/main/java/com/example/aspirushealthcareandroidapp/PatientSignUp.java
@@ -0,0 +1,45 @@
+package com.example.aspirushealthcareandroidapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.DatePickerDialog;
+import android.os.Bundle;
+import android.text.InputType;
+import android.view.View;
+import android.widget.DatePicker;
+import com.google.android.material.textfield.TextInputLayout;
+
+import java.util.Calendar;
+
+public class PatientSignUp extends AppCompatActivity {
+
+    DatePickerDialog calendar;
+    TextInputLayout dob;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_patient_sign_up);
+
+        dob = findViewById(R.id.signup_dob);
+        dob.getEditText().setInputType(InputType.TYPE_NULL);
+        dob.getEditText().setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                final Calendar cldr = Calendar.getInstance();
+                int day = cldr.get(Calendar.DAY_OF_MONTH);
+                int month = cldr.get(Calendar.MONTH);
+                int year = cldr.get(Calendar.YEAR);
+                // date picker dialog
+                calendar = new DatePickerDialog(PatientSignUp.this, new DatePickerDialog.OnDateSetListener() {
+                    @Override
+                    public void onDateSet(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
+                        dob.getEditText().setText(dayOfMonth + "/" + (monthOfYear + 1) + "/" + year);
+                    }
+                }, year, month, day);
+                calendar.show();
+            }
+        });
+    }
+
+}
