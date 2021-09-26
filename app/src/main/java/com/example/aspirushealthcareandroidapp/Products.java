b19730fd4f70852b795fb38bd8b98519a794f53a
Tharindu Dasun <tharindudasun0@gmail.com>
2021-09-26 17:34:36 +0530

    Completed User Pharmacy page

--- app/src/main/java/com/example/aspirushealthcareandroidapp/Products.java
+++ app/src/main/java/com/example/aspirushealthcareandroidapp/Products.java
@@ -1,7 +1,10 @@
 package com.example.aspirushealthcareandroidapp;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -10,18 +13,18 @@ import android.widget.EditText;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.GridLayoutManager;
-import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.firebase.ui.database.FirebaseRecyclerAdapter;
 import com.firebase.ui.database.FirebaseRecyclerOptions;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
 import com.squareup.picasso.Picasso;
 
 public class Products extends AppCompatActivity {
 
-    //EditText inputSearch;
+    EditText inputSearch;
     RecyclerView recyclerView;
     FirebaseRecyclerOptions<ProductModel>options; //pass the model class
     FirebaseRecyclerAdapter<ProductModel,ProductViewHolder>adapter;
@@ -35,56 +38,94 @@ public class Products extends AppCompatActivity {
         setContentView(R.layout.activity_products);
 
         ProductRef = FirebaseDatabase.getInstance().getReference().child("Products");
-        //inputSearch=findViewById(R.id.inputSeacrh);
+        inputSearch=findViewById(R.id.inputSeacrh);
         recyclerView=(RecyclerView) findViewById(R.id.recylerView);
 
         //LinearLayoutManager horizontalLayoutManagaer = new LinearLayoutManager(Products.this, LinearLayoutManager.HORIZONTAL, false);
         //recyclerView.setLayoutManager(horizontalLayoutManagaer);
 
         GridLayoutManager gridLayoutManager = new GridLayoutManager(this, 2);
-       recyclerView.setLayoutManager(gridLayoutManager);
+        recyclerView.setLayoutManager(gridLayoutManager);
 
         recyclerView.setHasFixedSize(true);
 
-        LoadData();
-        
+
+        // search function in the load data method
+        LoadData("");
+        inputSearch.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+                if(editable.toString()!=null){
+                    LoadData(editable.toString());
+                }
+                else {
+                    LoadData("");
+                }
+
+            }
+        });
+
 
     }
 
-    private void LoadData() {
-      options = new  FirebaseRecyclerOptions.Builder<ProductModel>().setQuery(ProductRef,ProductModel.class).build();
-      adapter = new FirebaseRecyclerAdapter<ProductModel, ProductViewHolder>(options) {
-          @Override
-          protected void onBindViewHolder(@NonNull ProductViewHolder holder,   int position, @NonNull ProductModel model) {
+    private void LoadData(String data)
+    {
+        //create the query for the search
 
-              holder.productName.setText(model.getProductName());
-              holder.price.setText("Rs "+ model.getPrice()+".00");
-              Picasso.get().load(model.getImage()).into(holder.imageView);
-
-              holder.view.setOnClickListener(new View.OnClickListener() {
-                  @Override
-                  public void onClick(View view) {
-                      Intent intent = new Intent(Products.this,ProductOneView.class);
-                      intent.putExtra("ItemKey",getRef(position).getKey());
-
-                      startActivity(intent);
-                  }
-              });
+        Query query = ProductRef.orderByChild("productName").startAt(data).endAt(data+"\uf8ff");
 
 
 
+        options = new  FirebaseRecyclerOptions.Builder<ProductModel>().setQuery(query,ProductModel.class).build();
+        adapter = new FirebaseRecyclerAdapter<ProductModel, ProductViewHolder>(options) {
+            @Override
+            protected void onBindViewHolder(@NonNull ProductViewHolder holder, @SuppressLint("RecyclerView")
+                    int position, @NonNull ProductModel model) {
 
-          }
+                holder.productName.setText(model.getProductName());
+                holder.price.setText("Rs "+ model.getPrice()+".00");
+                Picasso.get().load(model.getImage()).into(holder.imageView);
 
-          @NonNull
-          @Override
-          public ProductViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+                holder.view.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        Intent intent = new Intent(Products.this,ProductOneView.class);
+                        intent.putExtra("ItemKey",getRef(position).getKey());
 
-              View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.product_listview,parent,false);
-              return new ProductViewHolder(view);
-          }
-      };
-      adapter.startListening();
-      recyclerView.setAdapter(adapter);
+                        startActivity(intent);
+                    }
+                });
+
+
+
+
+            }
+
+            @NonNull
+            @Override
+            public ProductViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+
+                View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.product_listview,
+                        parent,false);
+                return new ProductViewHolder(view);
+            }
+        };
+        adapter.startListening();
+        recyclerView.setAdapter(adapter);
     }
-}
+
+    public void homePage(View view) {
+        Intent intent = new Intent(this,AdminPharmacy.class);
+        startActivity(intent);
+    }
+ }
