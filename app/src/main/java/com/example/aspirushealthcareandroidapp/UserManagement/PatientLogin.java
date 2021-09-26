517b743b9e3492bf9ea8f9da9326ce723bb1f97e
Minindu Senadheera <69930656+MininduSenadheera@users.noreply.github.com>
2021-09-26 17:27:22 +0530

    created the firebase login

--- app/src/main/java/com/example/aspirushealthcareandroidapp/UserManagement/PatientLogin.java
+++ app/src/main/java/com/example/aspirushealthcareandroidapp/UserManagement/PatientLogin.java
@@ -1,5 +1,6 @@
-package com.example.aspirushealthcareandroidapp;
+package com.example.aspirushealthcareandroidapp.UserManagement;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
@@ -8,8 +9,15 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.example.aspirushealthcareandroidapp.MainActivity;
+import com.example.aspirushealthcareandroidapp.R;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class PatientLogin extends AppCompatActivity {
 
@@ -20,12 +28,16 @@ public class PatientLogin extends AppCompatActivity {
     FloatingActionButton fab_google;
     FloatingActionButton fab_facebook;
     FloatingActionButton fab_twitter;
+    FirebaseAuth firebaseAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_patient_login);
 
+        firebaseAuth = FirebaseAuth.getInstance();
+
+        //linking the views
         et_email       = findViewById(R.id.login_email);
         et_password    = findViewById(R.id.login_password);
         tv_loginSignUp = findViewById(R.id.tv_loginsignup);
@@ -33,6 +45,17 @@ public class PatientLogin extends AppCompatActivity {
         fab_google     = findViewById(R.id.fab_google);
         fab_facebook   = findViewById(R.id.fab_facebook);
         fab_twitter    = findViewById(R.id.fab_twitter);
+
+        //animations for floating action buttons
+        fab_google.setTranslationY(300);
+        fab_facebook.setTranslationY(300);
+        fab_twitter.setTranslationY(300);
+        fab_google.setAlpha(0F);
+        fab_facebook.setAlpha(0F);
+        fab_twitter.setAlpha(0F);
+        fab_google.animate().translationY(0).alpha(1).setDuration(1000).setStartDelay(100).start();
+        fab_facebook.animate().translationY(0).alpha(1).setDuration(1000).setStartDelay(200).start();
+        fab_twitter.animate().translationY(0).alpha(1).setDuration(1000).setStartDelay(300).start();
 
         //link to sign up screen
         tv_loginSignUp.setOnClickListener(new View.OnClickListener(){
@@ -74,11 +97,46 @@ public class PatientLogin extends AppCompatActivity {
         });
     }
 
+    //check whether an user is already logged in
+    @Override
+    protected void onStart(){
+        super.onStart();
+        if(firebaseAuth.getCurrentUser() != null){
+            startActivity(new Intent(getApplicationContext(), MainActivity.class));
+            finish();
+        }
+    }
+
     public void SignIn(){
         String email = et_email.getText().toString();
         String password = et_password.getText().toString();
 
+        if(email.isEmpty()){
+            et_email.setError("Required");
+            return;
+        } else {
+            et_email.setError(null);
+        }
 
+        if(password.isEmpty()){
+            et_password.setError("Required");
+            return;
+        } else {
+            et_password.setError(null);
+        }
+
+        firebaseAuth.signInWithEmailAndPassword(email,password).addOnSuccessListener(new OnSuccessListener<AuthResult>() {
+            @Override
+            public void onSuccess(AuthResult authResult) {
+                startActivity(new Intent(getApplicationContext(), MainActivity.class));
+                finish();
+            }
+        }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                Toast.makeText(PatientLogin.this, e.getMessage(),Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 
     public void googleSignIn(){
