530bfefe0ccee34d4f6fa902ebfea6151ff38102
Minindu Senadheera <69930656+MininduSenadheera@users.noreply.github.com>
2021-09-29 20:27:46 +0530

    created doctor model

--- app/src/main/java/com/example/aspirushealthcareandroidapp/UserManagement/Doctor.java
+++ app/src/main/java/com/example/aspirushealthcareandroidapp/UserManagement/Doctor.java
@@ -5,4 +5,38 @@ public class Doctor extends Person {
     private String workingPlace;
     private double fee;
 
+    public Doctor() {
+    }
+
+    public Doctor(String userID, String username, String email, String phone, String dob, int age, String gender, String password, String image, String speciality,
+                  String workingPlace, double fee) {
+        super(userID, username, email, phone, dob, age, gender, password, image);
+        this.speciality = speciality;
+        this.workingPlace = workingPlace;
+        this.fee = fee;
+    }
+
+    public String getSpeciality() {
+        return speciality;
+    }
+
+    public void setSpeciality(String speciality) {
+        this.speciality = speciality;
+    }
+
+    public String getWorkingPlace() {
+        return workingPlace;
+    }
+
+    public void setWorkingPlace(String workingPlace) {
+        this.workingPlace = workingPlace;
+    }
+
+    public double getFee() {
+        return fee;
+    }
+
+    public void setFee(double fee) {
+        this.fee = fee;
+    }
 }
