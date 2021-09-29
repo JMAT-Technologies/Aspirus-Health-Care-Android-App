aef7979b67f6a68e8f45168b512a8b43e2f8c470
Joshua Amarasinghe <joshuaamarasinghe@gmail.com>
2021-09-30 02:39:23 +0530

    Updated Appointment Adapter

--- app/src/main/java/com/example/aspirushealthcareandroidapp/AppointmentAdapter.java
+++ app/src/main/java/com/example/aspirushealthcareandroidapp/AppointmentAdapter.java
@@ -6,6 +6,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 import androidx.annotation.NonNull;
@@ -13,10 +14,15 @@ import androidx.appcompat.app.AlertDialog;
 import androidx.recyclerview.widget.RecyclerView;
 import com.firebase.ui.database.FirebaseRecyclerAdapter;
 import com.firebase.ui.database.FirebaseRecyclerOptions;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.core.Context;
+import com.orhanobut.dialogplus.DialogPlus;
+import com.orhanobut.dialogplus.ViewHolder;
 
-
+import java.util.HashMap;
+import java.util.Map;
 
 public class AppointmentAdapter extends FirebaseRecyclerAdapter<AppointmentModel, AppointmentAdapter.myViewHolder> {
 
@@ -38,6 +44,59 @@ public class AppointmentAdapter extends FirebaseRecyclerAdapter<AppointmentModel
         holder.doctorName.setText(model.getDoctorName());
         holder.date.setText(model.getDate());
         holder.time.setText(model.getTime());
+
+
+        //Edit
+        holder.btnEdit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                final DialogPlus dialogPlus = DialogPlus.newDialog(holder.btnEdit.getContext())
+                        .setContentHolder(new ViewHolder(R.layout.dialog_appointment_update))
+                        .setExpanded(true,600)
+                        .create();
+
+                View myView = dialogPlus.getHolderView();
+                TextView tv_doctorName = myView.findViewById(R.id.tv_doctorName);
+                EditText time = myView.findViewById(R.id.et_appointmentUpdateTime);
+                EditText date = myView.findViewById(R.id.et_appointmentUpdateDate);
+
+                Button btn_appointmmentUpdate = myView.findViewById(R.id.btn_appointmmentUpdate);
+
+                tv_doctorName.setText(model.getDoctorName());
+                time.setText(model.getTime());
+                date.setText(model.getDate());
+
+                dialogPlus.show();
+
+
+                    //Update
+                    btn_appointmmentUpdate.setOnClickListener(new View.OnClickListener() {
+                        @Override
+                        public void onClick(View view) {
+                            Map<String,Object> map=new HashMap<>();
+                            map.put("time",time.getText().toString());
+                            map.put("date",date.getText().toString());
+
+                            FirebaseDatabase.getInstance().getReference().child("Appointments")
+                                .child(getRef(position).getKey()).updateChildren(map)
+                                .addOnSuccessListener(new OnSuccessListener<Void>() {
+                                    @Override
+                                    public void onSuccess(Void unused) {
+                                        Toast.makeText(holder.doctorName.getContext(), "Appointment Updated", Toast.LENGTH_SHORT).show();
+                                        dialogPlus.dismiss();
+                                    }
+
+                                })
+                                .addOnFailureListener(new OnFailureListener() {
+                                    @Override
+                                    public void onFailure( Exception e) {
+                                        dialogPlus.dismiss();
+                                    }
+                                });
+                        }
+                    });
+            }
+        });
 
         holder.btnDelete.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -77,6 +136,7 @@ public class AppointmentAdapter extends FirebaseRecyclerAdapter<AppointmentModel
 
         TextView doctorName,date,time;
         Button btnDelete;
+        Button btnEdit;
 
         public myViewHolder(@NonNull View itemView) {
             super(itemView);
@@ -85,6 +145,7 @@ public class AppointmentAdapter extends FirebaseRecyclerAdapter<AppointmentModel
             date=(TextView) itemView.findViewById(R.id.appointment_date);
             time=(TextView) itemView.findViewById(R.id.appointment_time);
 
+            btnEdit = (Button) itemView.findViewById(R.id.btnEdit);
             btnDelete = (Button) itemView.findViewById(R.id.btnDelete);
         }
     }
