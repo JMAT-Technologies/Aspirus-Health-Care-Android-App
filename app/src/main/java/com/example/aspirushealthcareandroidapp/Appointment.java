1fa265b1f4902a80ab9c950344f87fe067bb72ed
Joshua Amarasinghe <joshuaamarasinghe@gmail.com>
2021-09-30 22:42:45 +0530

    Added Bottom navigation bar

--- app/src/main/java/com/example/aspirushealthcareandroidapp/Appointment.java
+++ app/src/main/java/com/example/aspirushealthcareandroidapp/Appointment.java
@@ -2,7 +2,11 @@ package com.example.aspirushealthcareandroidapp;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.EditText;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
@@ -11,7 +15,12 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import com.firebase.ui.database.FirebaseRecyclerOptions;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
+import com.google.firebase.database.ValueEventListener;
 
 public class Appointment extends AppCompatActivity {
 
@@ -22,20 +31,18 @@ public class Appointment extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_appointment);
-        recyclerView = (RecyclerView)findViewById(R.id.appointmentRecyclerView);
 
+        recyclerView = (RecyclerView)findViewById(R.id.appointmentRecyclerView);
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
 
-        FirebaseRecyclerOptions<AppointmentModel> options =
-                new FirebaseRecyclerOptions.Builder<AppointmentModel>()
-                        .setQuery(FirebaseDatabase.getInstance().getReference().child("Appointments"), AppointmentModel.class)
-                        .build();
+        FirebaseRecyclerOptions<AppointmentModel> options = new FirebaseRecyclerOptions.Builder<AppointmentModel>().setQuery(FirebaseDatabase.getInstance().getReference().child("Appointments"), AppointmentModel.class).build();
 
         appointmentAdapter = new AppointmentAdapter(options);
         recyclerView.setAdapter(appointmentAdapter);
 
         //Navigation
         BottomNavigationView bottomNavigationView=findViewById(R.id.bottom_navigation);
+        bottomNavigationView.setSelectedItemId((R.id.profilepage));
         bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
             @Override
             public boolean onNavigationItemSelected(@NonNull MenuItem item) {
@@ -45,16 +52,16 @@ public class Appointment extends AppCompatActivity {
                         overridePendingTransition(0,0);
                         return true;
                     case R.id.pharmacypage:
-//                        startActivity(new Intent(getApplicationContext() , Pharmacy.class));
-//                        overridePendingTransition(0,0);
+                        startActivity(new Intent(getApplicationContext() , Pharmacy.class));
+                        overridePendingTransition(0,0);
                         return true;
                     case R.id.channelingpage:
                         startActivity(new Intent(getApplicationContext() , Channeling.class));
                         overridePendingTransition(0,0);
                         return true;
                     case R.id.cartpage:
-//                        startActivity(new Intent(getApplicationContext() , CartActivity.class));
-//                        overridePendingTransition(0,0);
+                        startActivity(new Intent(getApplicationContext() , CartActivity.class));
+                        overridePendingTransition(0,0);
                         return true;
                     case R.id.profilepage:
                         startActivity(new Intent(getApplicationContext() , PatientProfile.class));
