1f8874bb918a32de651ab20978e0bedef8e51680
Joshua Amarasinghe <joshuaamarasinghe@gmail.com>
2021-10-01 02:57:49 +0530

    Cleared

--- app/src/main/java/com/example/aspirushealthcareandroidapp/MainActivity.java
+++ app/src/main/java/com/example/aspirushealthcareandroidapp/MainActivity.java
@@ -2,40 +2,16 @@ package com.example.aspirushealthcareandroidapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
 
-import com.example.aspirushealthcareandroidapp.UserManagement.PatientLogin;
-import com.example.aspirushealthcareandroidapp.UserManagement.PatientProfile;
-import com.google.firebase.auth.FirebaseAuth;
 
 public class MainActivity extends AppCompatActivity {
-
-    Button btn_profile;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        btn_profile = findViewById(R.id.btn_logout2);
 
-        //profile function
-        btn_profile.setOnClickListener(new View.OnClickListener(){
-            @Override
-            public void onClick(View view){
-                startActivity(new Intent(getApplicationContext(), PatientProfile.class));
-            }
-        });
-    }
-
-    @Override
-    protected void onStart() {
-        super.onStart();
-        if(FirebaseAuth.getInstance().getCurrentUser() == null){
-            startActivity(new Intent(getApplicationContext(), PatientLogin.class));
-        }
     }
 }
