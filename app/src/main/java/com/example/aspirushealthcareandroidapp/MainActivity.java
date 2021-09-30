6c8efd407c88b89320040b698b86b629fad912b2
Minindu Senadheera <69930656+MininduSenadheera@users.noreply.github.com>
2021-10-01 01:47:19 +0530

    clear the main activity

--- app/src/main/java/com/example/aspirushealthcareandroidapp/MainActivity.java
+++ app/src/main/java/com/example/aspirushealthcareandroidapp/MainActivity.java
@@ -4,31 +4,17 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
 
-import com.example.aspirushealthcareandroidapp.UserManagement.PatientLogin;
-import com.example.aspirushealthcareandroidapp.UserManagement.PatientProfile;
+import com.example.aspirushealthcareandroidapp.UserManagement.Patient.PatientLogin;
 import com.google.firebase.auth.FirebaseAuth;
 
 public class MainActivity extends AppCompatActivity {
-
-    Button btn_profile;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        btn_profile = findViewById(R.id.btn_logout2);
-
-        //profile function
-        btn_profile.setOnClickListener(new View.OnClickListener(){
-            @Override
-            public void onClick(View view){
-                startActivity(new Intent(getApplicationContext(), PatientProfile.class));
-            }
-        });
     }
 
     @Override
