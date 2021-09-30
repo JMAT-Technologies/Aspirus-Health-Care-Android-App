57eda2177cde8ed25f9da75f42d9b73a877a95b6
Tharindu Dasun <tharindudasun0@gmail.com>
2021-10-01 01:55:39 +0530

    created the splash screen

--- app/src/main/java/com/example/aspirushealthcareandroidapp/MainActivity.java
+++ app/src/main/java/com/example/aspirushealthcareandroidapp/MainActivity.java
@@ -1,41 +1,38 @@
 package com.example.aspirushealthcareandroidapp;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
 
-import com.example.aspirushealthcareandroidapp.UserManagement.PatientLogin;
-import com.example.aspirushealthcareandroidapp.UserManagement.PatientProfile;
-import com.google.firebase.auth.FirebaseAuth;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.aspirushealthcareandroidapp.PharmacyManagement.Pharmacy;
+
+import java.util.Timer;
+import java.util.TimerTask;
 
 public class MainActivity extends AppCompatActivity {
-
-    Button btn_profile;
+    private  Timer splashTimer;
+    private static final long DELAY=2600;
+    private boolean schedule = false;
+    Context context;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        btn_profile = findViewById(R.id.btn_logout2);
+        context = this;
+        splashTimer = new Timer();
 
-        //profile function
-        btn_profile.setOnClickListener(new View.OnClickListener(){
+        splashTimer.schedule(new TimerTask() {
             @Override
-            public void onClick(View view){
-                startActivity(new Intent(getApplicationContext(), PatientProfile.class));
+            public void run() {
+                Intent intent = new Intent(context, Homepage.class);
+                startActivity(intent);
             }
-        });
-    }
+        },DELAY);
+        schedule = true ;
 
-    @Override
-    protected void onStart() {
-        super.onStart();
-        if(FirebaseAuth.getInstance().getCurrentUser() == null){
-            startActivity(new Intent(getApplicationContext(), PatientLogin.class));
-        }
     }
 }
