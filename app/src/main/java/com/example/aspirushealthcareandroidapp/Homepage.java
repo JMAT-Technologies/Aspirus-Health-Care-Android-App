a27e1aae9747c95b536810e224d795b59f3a3db4
Joshua Amarasinghe <joshuaamarasinghe@gmail.com>
2021-09-30 22:56:03 +0530

    Added Bottom Navigation Bar to Homepage

--- app/src/main/java/com/example/aspirushealthcareandroidapp/Homepage.java
+++ app/src/main/java/com/example/aspirushealthcareandroidapp/Homepage.java
@@ -1,8 +1,13 @@
 package com.example.aspirushealthcareandroidapp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class Homepage extends AppCompatActivity {
 
@@ -10,5 +15,34 @@ public class Homepage extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_homepage);
+
+        BottomNavigationView bottomNavigationView=findViewById(R.id.bottom_navigation);
+
+        bottomNavigationView.setSelectedItemId((R.id.homepage));
+
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch (item.getItemId()){
+                    case R.id.homepage:
+                        return true;
+                    case R.id.channelingpage:
+                        startActivity(new Intent(getApplicationContext()
+                                ,Channeling.class));
+                        overridePendingTransition(0,0);
+                        return true;
+                    case R.id.pharmacypage:
+                        startActivity(new Intent(getApplicationContext()
+                                ,Appointment.class));
+                        overridePendingTransition(0,0);
+                        return true;
+
+                }
+                return false;
+            }
+        });
+
+
+
     }
 }
